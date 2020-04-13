e54c949e7f00fcaa90bd44affd43a345b4ba3b0e
Kaelyn Rieffer <kaelynrieffer@gmail.com>
2020-04-13 14:42:45 -0700

    organized variables & added do while loop

--- 03-Homework/Develop/script.js
+++ 03-Homework/Develop/script.js
@@ -1,42 +1,41 @@
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// alert prompts go here
-
-// when button pressed do this- fix it
-  var generateBtn= alert("Please answer the following promts to generate the desired criteria for your random passowrd!")
-
-// pass length 
-  var name= prompt("Please enter a number between 8-128 to determine how long you would like your password.")
-    // need something to validate a number was entered and it was between designated peramiters.
-
-
-// character types
-  var name =confirm("would you like lower case characters?")
-  var name =confirm("Would you like upper case characters?")
-  var name =confirm("Would you like any numbers in your password?")
-  var name =confirm("Would you like any special characters in your password?")
-
-// validate input so that at least 1 character type was selected otherwise promt them invalide input try again.
-// validate input the user imput is correct and what they wanted
-
 // Write password to the #password input
 function writePassword() {
+  var greeting= alert("Please answer the following promts to generate the desired criteria for your random passowrd!");
+  do { 
+    var length= prompt("Please enter a number between 8-128 to determine how long you would like your password.");
+    var between= parseInt(length); 
+    if (between <8 || between >128){
+     alert ("Invalid response please try again!");
+    }
+  }
+  while (between <8 || between >128);
+  
+  var lower =confirm("would you like lower case characters?");
+  var upper =confirm("Would you like upper case characters?");
+  var numbers =confirm("Would you like any numbers in your password?");
+  var special =confirm("Would you like any special characters in your password?");
+
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
+
+
 }
 
+
 // answer to promts go here 
-
-
-
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+// validate input so that at least 1 character type was selected otherwise promt them invalide input try again.
+// validate input the user imput is correct and what they wanted
 
 // take imput and give output. 
 
